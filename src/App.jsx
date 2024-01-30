fb389c4ae371d328fa0da4100d2d267b5e491305
Anna Maria Rodrigues <42442977+amrodrigues@users.noreply.github.com>
2024-01-30 14:38:06 +0000

    usando o Javascrpit para criar um array e ultiizar o map para criar os Cards apenas com uma linha de código

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,18 @@ function App() {
     name: 'Baby Wizard',
     image: 'https://rickandmortyapi.com/api/character/avatar/31.jpeg'
   }
+
+  const item3 = {
+    name: 'Summer Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  }
+  const itens = [ item1, item2, item3]
  return( <>
  <div className="cards">
-     <Card item={item1} />
-     <Card item={item2}/>
+  {/* <Card item={item1} />
+     <Card item={item2}/> */}
+
+   {itens.map( item=><Card item={item}/>)}
   </div>
   </>
   
